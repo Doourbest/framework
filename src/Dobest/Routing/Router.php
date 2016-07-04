846ca1fafb475835c91b7e09ea8c3df4d490041d
roxma <l>
2016-07-04 14:23:45 +0800

    简化路由 uri 解析。

--- src/Dobest/Routing/Router.php
+++ src/Dobest/Routing/Router.php
@@ -127,16 +127,11 @@ class Router {
     // detect true URI, inspired by CodeIgniter 2
     private static function detect_uri()
     {
-        $uri = $_SERVER['REQUEST_URI'];
-        if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
-            $uri = substr($uri, strlen($_SERVER['SCRIPT_NAME']));
-        } elseif (strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0) {
-            $uri = substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
-        }
-        if ($uri == '/' || empty($uri)) {
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        if ($uri == '' || $uri=='/') {
             return '/';
         }
-        $uri = parse_url($uri, PHP_URL_PATH);
         return str_replace(array('//', '../'), '/', rtrim($uri, '/'));
+
     }
 }
